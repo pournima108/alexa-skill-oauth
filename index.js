9634a2a2039669e8a64329096d030ec1a3ed2744
pournima108 <mishra.pournima108@gmail.com>
2018-04-30 16:35:44 +0530

    sixth commit

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+var JiraClient = require('jira-connector');
+ 
+var jira = new JiraClient( {
+    host: 'jenjinstudios.atlassian.net',
+    basic_auth: {
+        base64: 'U2lyVXNlck9mTmFtZTpQYXNzd29yZDEyMw=='
+    }
+});
+
 
 app.listen(port);
 console.log("Server Running Successfully at port " + port);
