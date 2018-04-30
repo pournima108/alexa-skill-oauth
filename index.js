1a21194bd53ce10693d237fb4e043570a5d2c6b0
pournima108 <mishra.pournima108@gmail.com>
2018-04-30 19:03:14 +0530

    8th commit

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+// var express = require('express');
 
-var app = express();
-var port = process.env.PORT || 3000;
-app.use(express.static(__dirname));
+var JiraApi = require('jira-client');
+// var bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// var app = express();
+// var port = process.env.PORT || 3000;
+// app.use(express.static(__dirname));
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 
-var JiraClient = require('jira-connector');
- 
-var jira = new JiraClient( {
-    host: 'jenjinstudios.atlassian.net',
-    basic_auth: {
-        base64: 'U2lyVXNlck9mTmFtZTpQYXNzd29yZDEyMw=='
-    }
+// With ES5
+console.log("done")
+console.log(JiraApi)
+
+//Initialize
+var jira = new JiraApi({
+  protocol: 'https',
+  host: 'pournima.atlassian.net',
+  username: 'pournimam@hexaware.com',
+  password: 'Pournima108',
+  apiVersion: '2',
+  strictSSL: true
 });
+console.log(JSON.stringify(jira));
 
+jira.findIssue(issueNumber)
+  .then(function(issue) {
+    console.log('Status: ' + issue.fields.status.name);
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
 
-app.listen(port);
-console.log("Server Running Successfully at port " + port);
+// app.listen(port);
+// console.log("Server Running Successfully at port " + port);
